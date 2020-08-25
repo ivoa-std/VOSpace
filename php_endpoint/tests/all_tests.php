f1ed743122a475340848f2161152169392d40682
Giuliano Taffoni <giuliano.taffoni@inaf.it>
2020-08-25 18:10:09 +0200

    Remove php_endpoint

--- php_endpoint/tests/all_tests.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('test_node.php');
-require_once('test_vospace.php');
-require_once('test_service.php');
-require_once('test_endpoint.php');
-require_once('test_auth.php');
-
-?>
